2ba57f7450f674411d0905cc1173099c4bc4e448
fidt16 <ppkm.vk@gmail.com>
2016-06-24 14:13:20 +0300

    Finished smooth movement of camera ( when using keyboard ).

--- Assets/UIcontrol/CameraControl.cs
+++ Assets/UIcontrol/CameraControl.cs
@@ -20,9 +20,9 @@ public class CameraControl : MonoBehaviour {
 
 		Movements();
 		
-		UnpressingKeys();
-
 		SmoothMovement();
+
+		gameObject.transform.Translate(new Vector3(speed[3] - speed[2], speed[0] - speed[1], 0f));
 	}
 
 	void SmoothMovement() {
@@ -30,7 +30,6 @@ public class CameraControl : MonoBehaviour {
 		//If TFSM = 0 then we stop moving.
 		if(TFSM <= 0) {
 
-			TFSM = 0;
 			for(int i = 0; i < 4; i++) {
 
 				speed[i] = 0;
@@ -41,73 +40,46 @@ public class CameraControl : MonoBehaviour {
 
 			for(int i = 0; i < 4; i++) {
 
-				//Debug.Log(speed[i] + " " + i);
-				//speed[i] /= 2;
+				if(speed[i] > 0) 
+					speed[i] *= 0.9f;
 			}
-		}
-	}
-
-	void UnpressingKeys() {
-
-		float defTFSM = 2f;
-
-		//Check if keys were unpressed.
-		if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S) && !(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A))) {
-
-			TFSM = defTFSM;
-			speed[2] = 0;
-			speed[3] = 0;			
-		} else if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S)) {
-
-			Debug.Log("GG");
-			speed[0] = 0;
-			speed[1] = 0;
-		}
-
-		if(Input.GetKeyUp(KeyCode.D) || Input.GetKey(KeyCode.A) && !(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))) {
-
-			TFSM = defTFSM;
-			
-			speed[0] = 0;
-			speed[1] = 0;
-		} else if(Input.GetKeyUp(KeyCode.D) || Input.GetKey(KeyCode.A)) {
-
-			speed[2] = 0;
-			speed[3] = 0;
 		}
 	}
 
 	void Movements() {
 
-		float speedOfCamera = 0.2f;
+		float speedOfCamera = 0.5f;
+		float defTFSM = 0.5f;
 
 		//Speed up
 		if(Input.GetKey(KeyCode.LeftShift)) {
 
-			speedOfCamera = 0.5f;
+			speedOfCamera = speedOfCamera * 1.5f;
 		}
 
 		//Movements 
-		if(Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 91f) {
-
-			speed[0] = speedOfCamera;
-			TFSM = 0f;
-		} else if(Input.GetKey(KeyCode.S) && gameObject.transform.position.y > 9f) {
+		if(Input.GetKey(KeyCode.S) && gameObject.transform.position.y > 9f) {
 
 			speed[1] = speedOfCamera;
-			TFSM = 0f;
+			speed[0] = 0f;
+			TFSM = defTFSM;
+		} else if(Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 91f) {
+
+			speed[0] = speedOfCamera;
+			speed[1] = 0f;
+			TFSM = defTFSM;
 		}
 
-		if(Input.GetKey(KeyCode.A) && gameObject.transform.position.x > 15.5f) {
-
-			speed[2] = speedOfCamera;
-			TFSM = 0f;
-		} else if(Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 82.5f) {
+		if(Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 82.5f) {
 
 			speed[3] = speedOfCamera;
-			TFSM = 0f;
-		}
+			speed[2] = 0f;
+			TFSM = defTFSM;
+		} else if(Input.GetKey(KeyCode.A) && gameObject.transform.position.x > 15.5f) {
 
-		gameObject.transform.Translate(new Vector3(speed[3] - speed[2], speed[0] - speed[1], 0f));
+			speed[2] = speedOfCamera;
+			speed[3] = 0f;
+			TFSM = defTFSM;
+		}
 	}
 }
