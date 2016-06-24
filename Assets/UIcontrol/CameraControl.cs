0ebdef3917355f268db9410846b05204a5ee8a37
fidt16 <ppkm.vk@gmail.com>
2016-06-24 18:48:25 +0300

    Finished camera movements.
    I've finished creating system that'd control all of your camera
    movements.
    1. You can use middle mouse button to move.
    2. You can use bounds of screen to move.
    3. You can use keyboard to move (WASD + SHIFT(SPEED UP)).

--- Assets/UIcontrol/CameraControl.cs
+++ Assets/UIcontrol/CameraControl.cs
@@ -3,26 +3,50 @@ using System.Collections;
 
 public class CameraControl : MonoBehaviour {
 
+	public GameObject cursor;
+
 	float[] speed = new float[4];// UP DOWN LEFT RIGHT
 	float TFSM = 0f;//time for smooth movement
+	float defTFSM = 0.5f;
+	float speedOfCamera = 0.5f;
+	float defSpeedOfCamera = 0.5f;
+	
+	Vector3 prevPos = new Vector3(0f, 0f, 0f);
 
 	void Update() {
 
-		moveCameraWithKeyboard();
-	}
+		MovementsWithKeyBoard();
+		MovementsWithMMB();
+		MovementsOnBounds();
 
-	void moveCameraWithMouse() {
-
-
-	}
-
-	void moveCameraWithKeyboard() {
-
-		Movements();
-		
 		SmoothMovement();
 
 		gameObject.transform.Translate(new Vector3(speed[3] - speed[2], speed[0] - speed[1], 0f));
+		normalizeCamera();
+	}	
+
+	//Check if camera went out of bounds
+	void normalizeCamera() {
+
+		Vector3 newPos = gameObject.transform.position;
+
+		if(gameObject.transform.position.x > 82f) {
+
+			newPos.x = 82f;
+		} else if(gameObject.transform.position.x < 17f) {
+
+			newPos.x = 17f;
+		}
+
+		if(gameObject.transform.position.y > 90f) {
+
+			newPos.y = 90f;
+		} else if(gameObject.transform.position.y < 9) {
+
+			newPos.y = 9f;
+		}
+
+		gameObject.transform.position = newPos;
 	}
 
 	void SmoothMovement() {
@@ -46,40 +70,112 @@ public class CameraControl : MonoBehaviour {
 		}
 	}
 
-	void Movements() {
+	void MovementsOnBounds() {
 
-		float speedOfCamera = 0.5f;
-		float defTFSM = 0.5f;
-
-		//Speed up
-		if(Input.GetKey(KeyCode.LeftShift)) {
-
-			speedOfCamera = speedOfCamera * 1.5f;
-		}
+		float width = 17.3f;
+		float height = 9.7f;
 
 		//Movements 
-		if(Input.GetKey(KeyCode.S) && gameObject.transform.position.y > 9f) {
+		if(cursor.transform.localPosition.y < -height) {
 
 			speed[1] = speedOfCamera;
 			speed[0] = 0f;
 			TFSM = defTFSM;
-		} else if(Input.GetKey(KeyCode.W) && gameObject.transform.position.y < 91f) {
+		} else if(cursor.transform.localPosition.y > height) {
+
+			speed[0] = speedOfCamera;
+			speed[1] = 0f;
+			TFSM = defTFSM;
+		} else if(cursor.transform.localPosition.x > width) {
+
+			speed[3] = speedOfCamera;
+			speed[2] = 0f;
+			TFSM = defTFSM;
+		} else if(cursor.transform.localPosition.x < -width) {
+
+			speed[2] = speedOfCamera;
+			speed[3] = 0f;
+			TFSM = defTFSM;
+		}
+	}
+
+	//MMB - middle mouse button.
+	void MovementsWithMMB() {
+
+		Vector3 curPos = Input.mousePosition;
+		float dist = 0f;
+		float speedModificator = 30;
+
+		if(!Input.GetKey(KeyCode.Mouse2)) {
+
+			prevPos = curPos;
+			return;
+		}
+
+		if(curPos.x - prevPos.x < 0) {
+
+			dist = (prevPos.x - curPos.x)/speedModificator;
+			speed[3] = dist;
+			speed[2] = 0f;
+			TFSM = defTFSM;
+		} else if(curPos.x - prevPos.x > 0) {
+			
+			dist = (curPos.x - prevPos.x)/speedModificator;
+			speed[2] = dist;
+			speed[3] = 0f;
+			TFSM = defTFSM;
+		}
+
+		if(curPos.y - prevPos.y < 0) {
+
+			dist = (prevPos.y - curPos.y)/speedModificator;
+			speed[0] = dist;
+			speed[1] = 0f;
+			TFSM = defTFSM;
+		} else if(curPos.y - prevPos.y > 0) {
+
+			dist = (curPos.y - prevPos.y)/speedModificator;
+			speed[1] = dist;
+			speed[0] = 0f;
+			TFSM = defTFSM;
+		}
+
+		prevPos = curPos;
+	}
+
+	void MovementsWithKeyBoard() {
+
+		//Speed up
+		if(Input.GetKey(KeyCode.LeftShift)) {
+
+			speedOfCamera *= 1.5f;
+		}
+
+		//Movements 
+		if(Input.GetKey(KeyCode.S)) {
+
+			speed[1] = speedOfCamera;
+			speed[0] = 0f;
+			TFSM = defTFSM;
+		} else if(Input.GetKey(KeyCode.W)) {
 
 			speed[0] = speedOfCamera;
 			speed[1] = 0f;
 			TFSM = defTFSM;
 		}
 
-		if(Input.GetKey(KeyCode.D) && gameObject.transform.position.x < 82.5f) {
+		if(Input.GetKey(KeyCode.D)) {
 
 			speed[3] = speedOfCamera;
 			speed[2] = 0f;
 			TFSM = defTFSM;
-		} else if(Input.GetKey(KeyCode.A) && gameObject.transform.position.x > 15.5f) {
+		} else if(Input.GetKey(KeyCode.A)) {
 
 			speed[2] = speedOfCamera;
 			speed[3] = 0f;
 			TFSM = defTFSM;
 		}
+
+		speedOfCamera = defSpeedOfCamera;
 	}
 }
